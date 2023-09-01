3b50d1471316dc44bf6db984a73c386d40c42e08
Naiara Pantuza <naiarapantuza@gmail.com>
2023-09-01 15:11:23 -0300

    primeiro commit

--- ChefDelivery/App/ContentView.swift
+++ ChefDelivery/App/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, Naiara!")
         }
         .padding()
     }
@@ -21,6 +21,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+      ContentView()
     }
 }
