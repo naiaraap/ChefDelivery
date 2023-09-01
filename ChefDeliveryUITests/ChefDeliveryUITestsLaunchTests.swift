8c0a79f5a15a82344bb200d870f2eec6c7e05e51
Naiara Pantuza <naiarapantuza@gmail.com>
2023-09-01 11:05:24 -0300

    Initial Commit

---
+++ ChefDeliveryUITests/ChefDeliveryUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ChefDeliveryUITestsLaunchTests.swift
+//  ChefDeliveryUITests
+//
+//  Created by Naiara de Almeida Pantuza on 01/09/23.
+//
+
+import XCTest
+
+final class ChefDeliveryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
