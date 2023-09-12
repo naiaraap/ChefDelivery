c31141fbbb581e85a81e77fe40444bf82ab66cbc
Naiara Pantuza <naiarapantuza@gmail.com>
2023-09-12 17:35:03 -0300

    tela de detalhes da loja em andamento

--- ChefDelivery/App/ContentView.swift
+++ ChefDelivery/App/ContentView.swift
@@ -9,25 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-      NavigationView {
-        VStack(spacing: 20) {
-              NavigationBar()
-              .padding(.horizontal, 15)
-            
-            ScrollView(.vertical, showsIndicators: false) {
-              VStack {
-                OrderTypeGridView()
-                CarouselTabView()
-                StoresContainerView()
-              }
+        NavigationView {
+            VStack(spacing: 20) {
+                NavigationBar()
+                    .padding(.horizontal, 15)
+
+                ScrollView(.vertical, showsIndicators: false) {
+                    VStack {
+                        OrderTypeGridView()
+                        CarouselTabView()
+                        StoresContainerView()
+                    }
+                }
             }
         }
-      }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-      ContentView()
+        ContentView()
     }
 }
--- ChefDelivery/StoresView/StoreDetailView.swift
+++ ChefDelivery/StoresView/StoreDetailView.swift
@@ -8,16 +8,51 @@
 import SwiftUI
 
 struct StoreDetailView: View {
-  
     let store: StoreType
-  
+
     var body: some View {
-      Text(store.name)
+        VStack(alignment: .leading) {
+            Image(store.headerImage)
+                .resizable()
+                .scaledToFit()
+
+            HStack {
+                Text(store.name)
+                    .font(.title)
+                    .fontWeight(.bold)
+
+                Spacer()
+
+                Image(store.logoImage)
+            }
+
+            .padding(.vertical, 8)
+            .padding(.horizontal)
+
+            HStack {
+                Text(store.location)
+
+                Spacer()
+
+                ForEach(Array(1 ... store.stars), id: \.self) { _ in
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                        .font(.caption)
+                }
+            }
+            .padding(.vertical, 8)
+            .padding(.horizontal)
+
+            Text("Produtos")
+                .font(.title2)
+                .fontWeight(.bold)
+                .padding()
+        }
     }
 }
 
 struct StoreDetailView_Previews: PreviewProvider {
     static var previews: some View {
-      StoreDetailView(store: storesMock[0])
+        StoreDetailView(store: storesMock[0])
     }
 }
